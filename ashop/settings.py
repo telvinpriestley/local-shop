3cae67048203104f25b15214112cb0412c07cb28
Telvin Priestley <afanwitelvin@gmail.com>
2024-08-11 00:17:32 +0100

    db config

--- ashop/settings.py
+++ ashop/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'ashop.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join('/tmp', 'db.sqlite3'),
     }
 }
 
